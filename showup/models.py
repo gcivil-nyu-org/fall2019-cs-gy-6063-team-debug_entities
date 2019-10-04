a1269e990e819bced8693c0847b3e36b2eccf034
vmoros <vmoros@gmail.com>
2019-10-04 17:23:02 -0400

    Created initial Django model for SeatGeek concerts. Issue #31.

--- showup/models.py
+++ showup/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Concert(models.Model):
+    datetime = models.DateTimeField()
+    venue_name = models.CharField(max_length = 100)
+    performer_names = models.CharField(max_length = 200)
+    event_url = models.URLField()
+    performer_image_url = models.URLField()
+
+    def __str__(self):
+        return performer_names + " at " + venue_name + " on " + datetime
