b2f0456eee125e182be0b10474f553ff57ec3be1
vmoros <vmoros@gmail.com>
2019-10-05 18:38:56 -0400

    Updated the case of the showupconfig class to all lowercase

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'showup.apps.ShowupConfig',
+    'showup.apps.showupconfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- showup/apps.py
+++ showup/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class ShowupConfig(AppConfig):
+class showupconfig(AppConfig):
     name = 'showup'
