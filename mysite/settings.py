8a65470cdbdf5a49a200645eb6714e0122868494
Mel Savich <mksavic@nyu.edu>
2019-10-07 14:50:00 -0400

    update ShowUpConfig to CamelCase

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'showup.apps.showupconfig',
+    'showup.apps.ShowUpConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- showup/apps.py
+++ showup/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class showupconfig(AppConfig):
+class ShowUpConfig(AppConfig):
     name = 'showup'
