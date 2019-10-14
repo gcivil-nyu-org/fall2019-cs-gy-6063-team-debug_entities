bd1b7a3e05983e68272c8d9b58c822420ea07b5e
vmoros <vmoros@gmail.com>
2019-10-14 13:12:08 -0400

    Added signup form to forms.py for allauth. Removed old manual signup and login forms.

--- showup/forms.py
+++ showup/forms.py
@@ -1,17 +1,19 @@
 from django import forms
-from .models import Users
-from django.forms import ModelForm
+from allauth.account.forms import SignupForm
 
 
-class RegisterForm(ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
-    class Meta:
-        model = Users
-        fields = ['firstName','lastName','dateOfBirth','gender','email','password']
+class CustomSignupForm(SignupForm):
+    first_name = forms.CharField(max_length=255, label='First Name')
+    last_name = forms.CharField(max_length=255, label='Last Name')
+    date_of_birth = forms.DateField()
+    gender = forms.CharField(max_length=255)
+    email = forms.EmailField()
 
-
-class LoginForm(ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
-    class Meta:
-        model = Users
-        fields = ['email','password']
+    def signup(self, request, user):
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.date_of_birth = self.cleaned_data['date_of_birth']
+        user.gender = self.cleaned_data['gender']
+        user.email = self.cleaned_data['email']
+        user.save()
+        return user
