0717e85fd945711ef845d60b25333d1f86e50c0c
Mel Savich <mksavic@nyu.edu>
2019-10-15 16:27:19 -0400

    fix formatting in models.py + views.py

--- showup/views.py
+++ showup/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from .models import Concert
 
+
 def home(request):
     return render(request, 'home.html')
+
 
 def events(request):
     if request.user.is_authenticated:
         events = Concert.objects.all()
-        return render(request,'events.html',{'events' : events})
+        return render(request, 'events.html', {'events': events})
     else:
-        return render(request,'home.html')
+        return render(request, 'home.html')
