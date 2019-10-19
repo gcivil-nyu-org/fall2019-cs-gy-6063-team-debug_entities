735c9562083c8305909b6e22c7ff18197f2b93e4
vmoros <vmoros@gmail.com>
2019-10-19 14:29:57 -0400

    Updating views.py to comply with black and flake8

--- showup/views.py
+++ showup/views.py
@@ -6,21 +6,23 @@ from django.contrib.auth.decorators import login_required
 def home(request):
     return render(request, "home.html")
 
+
 @login_required
 def events(request):
     events = Concert.objects.all()
 
     # User clicked "Interested" button.
-    if("interested" in request.GET):
+    if "interested" in request.GET:
         event_id = request.GET.get("interested")
         request.user.interested.add(event_id)
 
     # User clicked "Going" button.
-    if("going" in request.GET):
+    if "going" in request.GET:
         event_id = request.GET.get("going")
         request.user.going.add(event_id)
-    
+
     return render(request, "events.html", {"events": events})
+
 
 @login_required
 def user(request, id):
