9bda41758759b737953768853e585f24ea7d3aed
vmoros <vmoros@gmail.com>
2019-10-19 14:25:55 -0400

    Changing views.py to comply with black

--- showup/views.py
+++ showup/views.py
@@ -11,17 +11,17 @@ def events(request):
     events = Concert.objects.all()
 
     # User clicked "Interested" button.
-    if('interested' in request.GET):
-        event_id = request.GET.get('interested')
+    if("interested" in request.GET):
+        event_id = request.GET.get("interested")
         request.user.interested.add(event_id)
 
     # User clicked "Going" button.
-    if('going' in request.GET):
-        event_id = request.GET.get('going')
+    if("going" in request.GET):
+        event_id = request.GET.get("going")
         request.user.going.add(event_id)
     
-    return render(request, 'events.html', {'events': events})
+    return render(request, "events.html", {"events": events})
 
 @login_required
 def user(request, id):
-    return render(request, 'user.html')
+    return render(request, "user.html")
