e0fbede5900ee68c8f6d74d626f53be3f8a2c4fb
kavinshah <38910831+kavinshah@users.noreply.github.com>
2019-10-20 01:47:09 -0400

    borough filter done

--- showup/views.py
+++ showup/views.py
@@ -10,13 +10,13 @@ def events(request):
     if request.user.is_authenticated:
         events = Concert.objects.all()
         borough_choices = Concert.BOROUGH_CHOICES
-        borough_selected = borough_choices
         
         #User clicks "Filter"
         if('filter' in request.GET):
-            borough_selected =  request.GET.get('borough_all')
-            events = Concert.objects.filter(borough__in=borough_selected)
-        
+            #filter boroughs
+            if('boroughs' in request.GET):
+                events = events.filter(borough__in=request.GET.getlist('boroughs'))
+            
         # User clicked "Interested" button.
         if('interested' in request.GET):
             event_id = request.GET.get('interested')
@@ -27,6 +27,6 @@ def events(request):
             event_id = request.GET.get('going')
             request.user.going.add(event_id)
 
-        return render(request, 'events.html', {'events': events, 'borough_choices': borough_choices, 'outputtext' : borough_selected})
+        return render(request, 'events.html', {'events': events, 'borough_choices': borough_choices})
     else:
         return render(request, 'home.html')
