0afe47902319719f9515d1e641d77a1ea2e75d13
kavinshah <38910831+kavinshah@users.noreply.github.com>
2019-10-20 04:56:03 -0400

    venue filtering

--- showup/views.py
+++ showup/views.py
@@ -15,11 +15,21 @@ def get_performers():
     performer_names_choices.sort()
     return performer_names_choices
     
+def get_venues():
+    venue_name_choices = []
+    for venue in Concert.objects.all().values("venue_name"):
+            venue_name_choices.extend([venue["venue_name"]])
+    venue_name_choices = [name.strip(' ') for name in venue_name_choices]
+    venue_name_choices = list(set(venue_name_choices))
+    venue_name_choices.sort()
+    return venue_name_choices
+    
 def events(request):
     if request.user.is_authenticated:
         events = Concert.objects.all()
         borough_choices = Concert.BOROUGH_CHOICES
         performer_names_choices = get_performers()
+        venue_name_choices = get_venues()
 
         #User clicks "Filter"
         if('filter' in request.GET):
@@ -29,8 +39,11 @@ def events(request):
                 
             #filter performers. only 1 performer at the moment
             if("performers" in request.GET):
-                print(request.GET)
                 events = events.filter(performer_names__contains=request.GET["performers"])
+                
+            #filter venues
+            if("venues" in request.GET):
+                events = events.filter(venue_name__in=request.GET.getlist("venues"))
             
         # User clicked "Interested" button.
         if('interested' in request.GET):
@@ -42,6 +55,8 @@ def events(request):
             event_id = request.GET.get('going')
             request.user.going.add(event_id)
 
-        return render(request, 'events.html', {'events': events, 'borough_choices': borough_choices, 'performer_names_choices': performer_names_choices})
+        return render(request, 'events.html', {'events': events,
+            'borough_choices': borough_choices, 'performer_names_choices': performer_names_choices,
+            'venue_choices': venue_name_choices})
     else:
         return render(request, 'home.html')
