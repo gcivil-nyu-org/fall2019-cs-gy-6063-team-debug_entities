190435cd0c6489fb6f72bf446e86f985578ae8ba
vmoros <vmoros@gmail.com>
2019-10-22 16:54:06 -0400

    Added STATIC_ROOT value to settings.py to fix an error on Travis about that value not being present

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Adding Show Up email information. Emails to users will be sent from this account.
 EMAIL_USE_TLS = True
