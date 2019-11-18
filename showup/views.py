828429c3d4cabb6033e49175b4a6a22fdebf8640
kavinshah <38910831+kavinshah@users.noreply.github.com>
2019-11-18 18:01:40 -0500

    final

--- showup/views.py
+++ showup/views.py
@@ -102,7 +102,7 @@ def event_stack(request, eid):
     match = CustomUser.objects.get(id=request.user.id)
     popup = 0
     users = get_stack(request, eid)
-    squad_matches = Squad.objects.get(id=request.squad.id)
+    squad_matches = match.squad
 
     if request.method == "POST":  # user is submitting a swipe
         swipee_id = request.POST["swipee_id"]
@@ -137,7 +137,7 @@ def event_stack(request, eid):
         users = get_stack(request, eid)
 
         # squad_id
-        squad_id = request.squad.id
+        squad_id = match.squad.id
 
         # The squads that I swiped right on.
         i_swiped_right = [
