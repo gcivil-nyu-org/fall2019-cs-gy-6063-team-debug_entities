3a1f50202812164cb36757af9b338aacddf31d01
kavinshah <38910831+kavinshah@users.noreply.github.com>
2019-11-18 23:45:31 -0500

    build error

--- showup/migrations/0005_merge_20191118_2327.py
+++ showup/migrations/0005_merge_20191118_2327.py
@@ -6,8 +6,8 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('showup', '0004_merge_20191118_1643'),
-        ('showup', '0003_auto_20191118_1915'),
+        ("showup", "0004_merge_20191118_1643"),
+        ("showup", "0003_auto_20191118_1915"),
     ]
 
     operations = []
