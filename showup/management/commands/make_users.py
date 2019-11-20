cb6c9b7d3ff80b6bdaac98079ad046fc3f5dc57a
Viktor Moros <vm1564@nyu.edu>
2019-11-19 22:01:47 -0500

    Redid the logic of the make_users command to make it simpler. Also added Prof and Dan as users.

--- showup/management/commands/make_users.py
+++ showup/management/commands/make_users.py
@@ -4,27 +4,37 @@ from allauth.account.admin import EmailAddress
 
 
 class Command(BaseCommand):
-    def handle(self, *args, **options):
-        password = "heyhey123"
-        name = "Vik"
-        email = "vm1564@nyu.edu"
-
-        new_superuser = CustomUser.objects.create_superuser(  # make superuser
-            username=name, email=email, password=password, first_name=name
-        )
-        print()
-        EmailAddress.objects.get_or_create(  # verify superuser's email
-            user=new_superuser, email=email, verified=True
-        )
-        print(f"I made {name} as a superuser and verified {email}")
-
-        for i in range(1, 10):
-            email = f"vm1564+{i}@nyu.edu"
-            name = f"Vik{i}"
-            new_user = CustomUser.objects.create_user(  # make normal users
-                username=name, email=email, password=password, first_name=name
-            )
-            EmailAddress.objects.get_or_create(  # verify their emails
+    def make_user(name, email, superuser):
+        # superuser is a bool telling us if the user we make should be super or not
+        if CustomUser.objects.filter(email=email).exists():
+            print(f"We already have a user with the email address {email}")
+        else:
+            password = "heyhey123"
+            if superuser:
+                new_user = CustomUser.objects.create_superuser(
+                    username=name, email=email, password=password, first_name=name
+                )
+            else:
+                new_user = CustomUser.objects.create_user(
+                    username=name, email=email, password=password, first_name=name
+                )
+            EmailAddress.objects.get_or_create(  # verify the user's email
                 user=new_user, email=email, verified=True
             )
-            print(f"I made {name} as a normal user and verified {email}")
+
+            print(f"I made {name} as a {'super' if superuser else 'normal '}user"
+                f" and verified {email}")
+
+    def handle(self, *args, **options):
+        superusers = [
+            {"name": "Vik", "email": "vm1564@nyu.edu"},
+            {"name": "Professor", "email": "gcivil@nyu.edu"},
+            {"name": "Dan", "email": "dgopstein@nyu.edu"},
+        ]
+        for u in superusers:
+            Command.make_user(u["name"], u["email"], True)
+
+        normal_users = [{"name": f"Vik{i}", "email": f"vm1564+{i}@nyu.edu"}
+                        for i in range(1, 10)]
+        for u in normal_users:
+            Command.make_user(u["name"], u["email"], False)
