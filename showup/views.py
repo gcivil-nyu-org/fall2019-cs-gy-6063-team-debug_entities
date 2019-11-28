81f71a6522a38eb990e4933688e266334d17943f
vedanth dasari <vd908@nyu.edu>
2019-11-27 19:54:32 -0500

    Name is displayed more than once in matching stack bug fix

--- showup/views.py
+++ showup/views.py
@@ -231,8 +231,9 @@ def get_stack(request, eid):
     sid = request.user.squad.id
 
     # Get the squads interested in or going to the event.
-    squads = Squad.objects.filter(interested__id=eid) | Squad.objects.filter(
-        going__id=eid
+    squads = (
+        Squad.objects.filter(interested__id=eid).distinct()
+        | Squad.objects.filter(going__id=eid).distinct()
     )
 
     # These are the squads that swiped left on my squad.
