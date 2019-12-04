8f1e5a46937025b30382c8e499e816be61d54659
Mel Savich <mksavic@nyu.edu>
2019-12-04 14:53:28 -0500

    fix black error

--- showup/views.py
+++ showup/views.py
@@ -179,9 +179,7 @@ def edit_squad(request, id):
                     request.delete()
                 else:
                     # Create a request.
-                    Request.objects.create(
-                        requester=my_squad, requestee=their_squad
-                    )
+                    Request.objects.create(requester=my_squad, requestee=their_squad)
 
                 return redirect(reverse("squad", kwargs={"id": my_squad.id}))
 
