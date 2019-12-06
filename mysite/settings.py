f03402037f1c5ceaf63f0da2b9d52208b219a800
Viktor Moros <vm1564@nyu.edu>
2019-12-05 21:03:28 -0500

    Added whitenoise and associated settings to settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -59,6 +59,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "mysite.urls"
@@ -125,6 +126,8 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 # Media Files
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
